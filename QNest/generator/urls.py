82574b98937025796deac1a6b966d0c270e1c15e
Anjana2002 <anjanaa628@gmail.com>
2025-03-17 12:18:18 +0530

    add url

--- QNest/generator/urls.py
+++ QNest/generator/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, register, user_logout, upload_files
+from .views import home, register, user_logout, upload_files, create_template
 
 urlpatterns = [
     path("", home, name="home"), 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("logout/", user_logout, name="logout"),
     path("upload/", upload_files, name="upload_files"), 
     # path("download_pdf/", download_pdf, name="download_pdf"),
+    path("create-template/", create_template, name="create_template"),
 ]
